25ea4fd9d8c434052d56ff3b579de31b7de0022b
Ren911 <renat.dautov@gmail.com>
2014-04-07 15:28:58 +0400

    Начальная визуализация... G GRaph пока нет

--- src/AppWindow.java
+++ src/AppWindow.java
@@ -10,21 +10,10 @@ import java.awt.event.*;
 public class AppWindow extends Frame {
 
     public AppWindow(){
-        addWindowListener(new MyWindowAdapter());    // регистрация слушателей
+        addWindowListener(new MyWindowAdapter());    // регистрация оконного слушателя
     }
 
-    public static void main(String args[]){
-        AppWindow appwin = new AppWindow();       // создаем окно, устанавливаем имя, размер и делаем видимым.
-        appwin.setSize(800,600);
-        appwin.setTitle("An Ant visualization");
-        appwin.setVisible(true);
-
-        GNode gNode = new GNode();
-    }
-
-
-
-    class MyWindowAdapter extends WindowAdapter {    // закрытие окнаа при нажатии на крестик
+    class MyWindowAdapter extends WindowAdapter {    // закрытие окна при нажатии на крестик
         public void windowClosing(WindowEvent we) {
             System.exit(0);
         }
--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 import java.lang.*;
 
 
@@ -6,11 +8,31 @@ import java.lang.*;
  * Класс, контролирующий всю внутренню деятельность системы. То бишь main
  */
 public class Driver {
+
+    private Writer writer; // класс, занимающийся отрисовкой графа
+
+    public  Driver (GGraph graph) {
+        int fringe = 40; // размер рамки
+
+        // создаем writer
+        writer = new Writer(800, 600, fringe, graph);
+
+        // Panel  - в ней будет отрисовываться сам граф
+        JPanel graphPanel = new JPanel(new BorderLayout());
+        graphPanel.add(writer, BorderLayout.CENTER);
+
+        // оконное приложение, с которым происходит работа
+        AppWindow appwin = new AppWindow();           // создание окна
+        appwin.setSize(800, 600);                    // установка размера, названия и добавления панели отрисовки
+        appwin.setTitle("An Ant visualization");
+        appwin.add(graphPanel, BorderLayout.CENTER);
+        appwin.setVisible(true);                      // делаем видимым
+    }
+
+
     public static void main(String [ ] args){
-        GGraph Test = new GGraph(10,54);
-
-
-
+        GGraph testGGraph = new GGraph(10,54); // создание тестового графа
+        new Driver(testGGraph);                // визуализация тест. графа
         }
     }
 
