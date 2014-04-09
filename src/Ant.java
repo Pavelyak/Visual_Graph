c0f04dbfb4101aea0e79ad2163801d23679ba70f
Killon <p.yakovlev@phystech.edu>
2014-04-09 17:30:53 +0400

    Муравей в тестовом режиме бегает

--- src/Ant.java
+++ src/Ant.java
@@ -7,28 +7,33 @@ import java.util.Random;
 
 public class Ant {
 
-    private GNode currentNode ; //сделать указателем
-    private GNode nextNode;
-    private GNode startNode;
+    private GNode currentNode ;             //текущее местонахождение муравья
+    private GNode nextNode;                 // следующее
+    private GNode startNode;                // самая начальная точка
 
 
+
+    //конструктор муравья, в котором прописываем муравья,
+    //который бегает бесконечно долго и оставляет феромоны
     public Ant(GGraph graph){
-        startNodeGenerator(graph);
+        startNodeGenerator(graph);          //выбираем начальную точку
         currentNode = startNode;
-        putPheromon();
-        while(true){
-            chooseNextNode();
-            visitNextNode();
+        putPheromon();                      // оставляем феромончик
+        while(true){                        // бесконечный цикл
+            chooseNextNode();               // обхода вершин
+            visitNextNode();                // и оставление феромонов в них
             putPheromon();
         }
     }
 
+
+    //функция выбора следующей вершинки для движения муравья
     public void chooseNextNode(){
         Random rand = new Random();
         int intNextNode = rand.nextInt(currentNode.listOut.size()); //выбираем одну из вершин, куда далее пойдет муравей
         nextNode = currentNode.listOut.get(intNextNode);
     }
-
+    //функция выбора точки старта муравья
     public GNode startNodeGenerator(GGraph graph){
         Random rand = new Random();
         int intStartNode = rand.nextInt(graph.getgNodesArraySize());
@@ -36,9 +41,12 @@ public class Ant {
         return startNode;
     }
 
-    public void visitNextNode(){
+    //функция посещения следуюющей вершины
+      public void visitNextNode(){
         currentNode = nextNode;
     }
+
+    //функция, оставление феромона
     public void putPheromon(){
         currentNode.setPheromonLevel();
     }
